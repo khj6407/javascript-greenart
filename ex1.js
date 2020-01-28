40bb4afe5a73489b8ea39ed73d97d8c880ae4f49
khj6407 <khj64076407@gmail.com>
2020-01-28 11:01:08 +0900

    javascript practice

---
+++ ex1.js
@@ -0,0 +1,18 @@
+var txt1 = "text1";
+consolo.log(txt1);
+
+if (true) {
+  var txt2 = "text2";
+  console.log(txt2);
+}
+let txt3 = "text3";
+const txt4 = "kim";
+console.log(txt3);
+console.log(txt4);
+txt3 = "lee";
+console.log(txt3);
+if (true) {
+  let txt5 = "yang"; //같은 블록 안에서만 통용
+  console.log(txt5);
+}
+//console.log(txt5);
---
+++ ex3.js
@@ -0,0 +1,12 @@
+let level = "A";
+
+switch (level) {
+  case "A":
+    console.log("VIP LEVEL!");
+    break;
+  case "B":
+    console.log("BASIC LEVEL");
+    break;
+  default:
+    console.log("NOTHING LEVEL");
+}
